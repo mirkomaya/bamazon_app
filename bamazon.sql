69bdd815cedfeec8e7d8a0e5b20e61665be68d93
Marty McFly <mirko.maya@live.com>
2019-10-02 15:30:19 -0400

    fixed price issue.

--- bamazon.sql
+++ bamazon.sql
@@ -8,8 +8,8 @@ CREATE TABLE products(
   item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(100) NOT NULL,
   department_name VARCHAR(45) NOT NULL,
-  price INT default 0,
-  stock_quantity INT default 0,
+  price DECIMAL(10,2) NOT NULL,
+  stock_quantity DECIMAL(10) NOT NULL,
   PRIMARY KEY (item_id)
 );
 
