77339943b2ca2271b1477826d138e9af2dd17192
Marty McFly <mirko.maya@live.com>
2019-10-08 12:54:08 -0400

    Checking user input.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -41,26 +41,6 @@ Stock Quanity:   ${res[i].stock_quantity}
     });
 }
 
-// function displayMessages() {
-//     inquirer.prompt([
-//         {
-//             type: "input",
-//             name: "productID",
-//             message: "What is the Product ID of the product you would like to buy?",
-//         },
-
-//         {
-//             type: "input",
-//             name: "productAmt",
-//             message: "How many units would you like to buy?"
-
-//         }
-//     ]).then(function (answer) {
-//         console.log(answer);
-//         connection.end();
-//     });
-// }
-
 function productMessage() {
     inquirer
         .prompt([{
@@ -78,61 +58,24 @@ function productMessage() {
             // console.log(answer.productID);
             connection.query("SELECT * FROM products WHERE ?", { item_id: answer.productID }, function (err, res) {
                 if (err) throw err;
-//                 console.log(`
-// Product ID:      ${res[0].item_id}
-// Product Name:    ${res[0].product_name}
-// Department Name: ${res[0].department_name}
-// Price:           ${res[0].price}
-// Stock Quanity:   ${res[0].stock_quantity}
-//           `);
 
-                // connection.query("SELECT * FROM products WHERE ?", { stock_quantity: answer.productAmt }, function (err, res) {
-                //     if (err) throw err;
-                //     console.log(answer.productAmt)
-                //     //   runSearch();
-                //     // amountMessage();
-                //     connection.end();
-                // });
                 console.log(`
 Product Name: ${res[0].product_name}
 
 Stock Quanity: ${res[0].stock_quantity}
+
+Units to be purchased: ${answer.productAmt}
 `);
-                console.log("User would like to buy: " + answer.productAmt + " amount")
-                // console.log(`${res[0].stock_quantity} is left`)
+
+                if (answer.productAmt > res[0].stock_quantity) {
+                    console.log("Insufficient quantity!")
+                } else {
+                    console.log("We have enough items to fill your request")
+                };
                 connection.end();
-
             });
-
         })
 }
 
-// function amountMessage() {
-//     inquirer
-//         .prompt({
-//             type: "input",
-//             name: "productAmt",
-//             message: "How many units would you like to buy?"
-//         })
-//         .then(function (answer) {
-//             // console.log(answer.productAmt);
-//             connection.query("SELECT * FROM products WHERE ?", { stock_quantity: answer.productAmt }, function (err, res) {
-//                 if (err) throw err;
-//                 console.log(answer.productAmt);
-//                 console.log(res[0].stock_quantity);
-//                 //   runSearch();
-//                 connection.end();
-//             });
-//         });
-// }
-
-
-
-
-// Product ID:      ${res[i].item_id}
-// Product Name:    ${res[i].product_name}
-// Department Name: ${res[i].department_name}
-// Price:           ${res[i].price}
-// Stock Quanity:   ${res[i].stock_quantity}
 
 
